bd575ba3412208176fb9faa6398375fb327b4b0e
fei-ke <jinyang656@qq.com>
2022-08-17 19:47:17 +0800

    Tips when can't open open app list

--- app/src/main/java/one/yufz/hmspush/MainActivity.kt
+++ app/src/main/java/one/yufz/hmspush/MainActivity.kt
@@ -1,8 +1,12 @@
 package one.yufz.hmspush
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +15,19 @@ class MainActivity : Activity() {
             setClassName(HMS_PACKAGE_NAME, HMS_CORE_DUMMY_SETTINGS_ACTIVITY)
             putExtra(FLAG_HMS_PUSH_SETTINGS, true)
         }
-        startActivity(intent)
-        finish()
+        try {
+            startActivity(intent)
+            finish()
+        } catch (e: ActivityNotFoundException) {
+            inflateTips()
+        }
+    }
+
+    private fun inflateTips() {
+        window.statusBarColor = Color.TRANSPARENT
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        setContentView(R.layout.tips)
     }
 }
