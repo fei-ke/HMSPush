e8ab4bb74dc363e47ac5557134887ac11eee142e
fei-ke <jinyang656@qq.com>
2022-08-18 17:20:43 +0800

    Add log

--- app/src/main/java/one/yufz/hmspush/fakedevice/FakeDevice.kt
+++ app/src/main/java/one/yufz/hmspush/fakedevice/FakeDevice.kt
@@ -1,14 +1,18 @@
 package one.yufz.hmspush.fakedevice
 
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import one.yufz.hmspush.XLog
 
 object FakeDevice {
+    private const val TAG = "FakeDevice"
+
     private val Default = arrayOf(Common::class.java)
     private val FakeDeviceConfig: Map<String, Array<Class<out IFakeDevice>>> = mapOf(
         "com.coolapk.market" to arrayOf(CoolApk::class.java),
     )
 
     fun fake(lpparam: XC_LoadPackage.LoadPackageParam) {
+        XLog.d(TAG, "fake() called with: packageName = ${lpparam.packageName}, processName = ${lpparam.processName}")
         val fakes = FakeDeviceConfig[lpparam.packageName] ?: Default
         fakes.forEach { it.newInstance().fake(lpparam) }
     }
