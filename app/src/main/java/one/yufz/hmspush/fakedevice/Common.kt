65131dc088d2fa8fe55bc263f7dcbd7ab3a922c3
fei-ke <jinyang656@qq.com>
2022-08-21 11:53:28 +0800

    Fake device for command "getprop xxx"

--- app/src/main/java/one/yufz/hmspush/fakedevice/Common.kt
+++ app/src/main/java/one/yufz/hmspush/fakedevice/Common.kt
@@ -2,22 +2,28 @@ package one.yufz.hmspush.fakedevice
 
 import android.os.Build
 import de.robv.android.xposed.callbacks.XC_LoadPackage
-import one.yufz.hmspush.HookContext
-import one.yufz.hmspush.findClass
-import one.yufz.hmspush.hookMethod
-import one.yufz.hmspush.set
+import one.yufz.hmspush.*
 
 class Common : IFakeDevice {
+    companion object {
+        private const val TAG = "Common"
+
+        private val FAKE_PROPERTIES = mapOf(
+            "ro.build.hw_emui_api_level" to "21",
+            "ro.build.version.emui" to "EmotionUI",
+            "ro.product.brand" to "HUAWEI",
+            "ro.product.manufacturer" to "HUAWEI",
+            "ro.miui.ui.version.name" to "",
+        )
+    }
+
     override fun fake(lpparam: XC_LoadPackage.LoadPackageParam): Boolean {
         val classSystemProperties = lpparam.classLoader.findClass("android.os.SystemProperties")
         val callback: HookContext.() -> Unit = {
             doBefore {
                 val key = args[0] as String
-                when (key) {
-                    "ro.build.hw_emui_api_level" -> result = "21"
-                    "ro.build.version.emui" -> result = "EmotionUI"
-                    "ro.product.brand", "ro.product.manufacturer" -> result = "HUAWEI"
-                    "ro.miui.ui.version.name" -> result = ""
+                FAKE_PROPERTIES[key]?.let {
+                    result = it
                 }
             }
         }
@@ -26,6 +32,19 @@ class Common : IFakeDevice {
 
         Build::class.java["BRAND"] = "HUAWEI"
         Build::class.java["MANUFACTURER"] = "HUAWEI"
+
+        Runtime::class.java.hookMethod("exec", String::class.java) {
+            doBefore {
+                val cmd = args[0] as String
+                if (cmd.startsWith("getprop")) {
+                    val key = cmd.removePrefix("getprop").trim()
+                    FAKE_PROPERTIES[key]?.let {
+                        XLog.d(TAG, "hook getprop $key")
+                        args[0] = "echo $it"
+                    }
+                }
+            }
+        }
 
         return true
     }
