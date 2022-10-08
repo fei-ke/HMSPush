0bc6a244c5e4756e3080952d6843a04d01e6c034
fei-ke <jinyang656@qq.com>
2022-10-08 19:21:31 +0800

    Aync load app icon

--- app/src/main/java/one/yufz/hmspush/app/home/AppListScreen.kt
+++ app/src/main/java/one/yufz/hmspush/app/home/AppListScreen.kt
@@ -1,8 +1,8 @@
 package one.yufz.hmspush.app.home
 
+import android.content.Context
 import android.graphics.drawable.Drawable
 import android.text.format.DateUtils
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,19 +19,19 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -42,6 +42,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.drawablepainter.rememberDrawablePainter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import one.yufz.hmspush.BuildConfig
 import one.yufz.hmspush.R
 import one.yufz.hmspush.app.theme.AppTheme
@@ -59,7 +61,10 @@ fun AppListScreen(searchText: String, appListViewModel: AppListViewModel = viewM
 @Composable
 private fun AppList(appList: List<AppInfo>) {
     LazyColumn() {
-        items(appList) {
+        items(
+            items = appList,
+            key = { it.packageName }
+        ) {
             AppCard(it)
         }
     }
@@ -67,9 +72,8 @@ private fun AppList(appList: List<AppInfo>) {
 
 @Composable
 private fun AppCard(info: AppInfo) {
-    val context = LocalContext.current
-    val icon: Drawable? = context.packageManager.getApplicationIcon(info.packageName)
-    val drawablePainter = rememberDrawablePainter(drawable = icon)
+    val drawable by loadAppIcon(LocalContext.current, info.packageName)
+    val drawablePainter = rememberDrawablePainter(drawable)
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -121,6 +125,19 @@ private fun AppCard(info: AppInfo) {
         }
 
     }
+}
+
+@Composable
+private fun loadAppIcon(context: Context, packageName: String): MutableState<Drawable?> {
+    val drawable = remember { mutableStateOf<Drawable?>(null) }
+
+    LaunchedEffect(packageName) {
+        launch(Dispatchers.IO) {
+            drawable.value = context.packageManager.getApplicationIcon(packageName)
+        }
+    }
+
+    return drawable
 }
 
 @Composable
