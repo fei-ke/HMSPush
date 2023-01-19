05ca10b8582c75d7562b79a8e796546b45c58333
fei-ke <jinyang656@qq.com>
2023-01-19 12:23:38 +0800

    Optimize CoolApk registration

--- xposed/src/main/java/one/yufz/hmspush/hook/fakedevice/CoolApk.kt
+++ xposed/src/main/java/one/yufz/hmspush/hook/fakedevice/CoolApk.kt
@@ -1,13 +1,42 @@
 package one.yufz.hmspush.hook.fakedevice
 
 import android.app.Application
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import one.yufz.hmspush.hook.XLog
 import one.yufz.xposed.hookMethod
 
 class CoolApk : XGPush() {
+    companion object {
+        private const val TAG = "CoolApk"
+    }
+
     override fun fake(lpparam: XC_LoadPackage.LoadPackageParam): Boolean {
-        Application::class.java.hookMethod("onCreate") {
-            doAfter { super.fake(lpparam) }
+        Application::class.java.hookMethod("attach", Context::class.java) {
+            doAfter {
+                super.fake(lpparam)
+
+                val context = thisObject as Context
+                listOf(
+                    "com.huawei.agconnect.core.provider.AGConnectInitializeProvider",
+                    "com.huawei.agconnect.core.ServiceDiscovery",
+                    "com.tencent.android.hwpushv3.HWHmsMessageService",
+                    "com.huawei.hms.support.api.push.PushMsgReceiver",
+                    "com.huawei.hms.support.api.push.PushReceiver",
+                    "com.huawei.hms.support.api.push.PushProvider",
+                )
+                    .map { ComponentName(context, it) }
+                    .filter { context.packageManager.getComponentEnabledSetting(it) == PackageManager.COMPONENT_ENABLED_STATE_DISABLED }
+                    .forEach {
+                        try {
+                            context.packageManager.setComponentEnabledSetting(it, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, 0)
+                        } catch (t: Throwable) {
+                            XLog.d(TAG, t.message)
+                        }
+                    }
+            }
         }
         return true
     }
