4b9012835c6ca7d3236292bb2afdaa71ce27027b
fei-ke <jinyang656@gmail.com>
2023-03-05 15:40:26 +0800

    Hook HMS DeviceId

--- xposed/src/main/java/one/yufz/hmspush/hook/fakedevice/FakeDevice.kt
+++ xposed/src/main/java/one/yufz/hmspush/hook/fakedevice/FakeDevice.kt
@@ -1,12 +1,9 @@
 package one.yufz.hmspush.hook.fakedevice
 
-import android.app.Application
-import android.content.Context
-import android.content.ContextWrapper
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import one.yufz.hmspush.common.BridgeWrap
 import one.yufz.hmspush.hook.XLog
-import one.yufz.xposed.hookMethod
+import one.yufz.xposed.onApplicationAttachContext
 
 object FakeDevice {
     private const val TAG = "FakeDevice"
@@ -34,20 +31,20 @@ object FakeDevice {
         val fakes = FakeDeviceConfig[lpparam.packageName] ?: Default
         fakes.forEach { it.newInstance().fake(lpparam) }
 
-        ContextWrapper::class.java.hookMethod("attachBaseContext", Context::class.java) {
-            doAfter {
-                XLog.d(TAG, "${thisObject}.attachBaseContext() called")
-                try {
-                    if (thisObject is Application) {
-                        unhook()
-                        if (BridgeWrap.isDisableSignature(thisObject as Context)) {
-                            FakeHmsSignature.hook(lpparam)
-                        }
-                    }
-                } catch (t: Throwable) {
-                    XLog.e(TAG, "disable signature error", t)
+        fakeOthers(lpparam)
+    }
+
+    private fun fakeOthers(lpparam: XC_LoadPackage.LoadPackageParam) {
+        onApplicationAttachContext {
+            XLog.d(TAG, "${this}.attachBaseContext() called")
+            try {
+                if (BridgeWrap.isDisableSignature(this)) {
+                    FakeHmsSignature.hook(lpparam)
                 }
+            } catch (t: Throwable) {
+                XLog.e(TAG, "disable signature error", t)
             }
+            HookHmsDeviceId.hook(lpparam)
         }
     }
 }
---
+++ xposed/src/main/java/one/yufz/xposed/Android.kt
@@ -0,0 +1,40 @@
+package one.yufz.xposed
+
+import android.app.Application
+import android.content.Context
+import android.content.ContextWrapper
+import android.util.Log
+import dalvik.system.BaseDexClassLoader
+import de.robv.android.xposed.XC_MethodHook.Unhook
+import one.yufz.hmspush.common.doOnce
+import one.yufz.hmspush.hook.XLog
+
+private const val TAG = "AndroidHookUtils"
+
+fun onApplicationAttachContext(callback: Application.() -> Unit) {
+    ContextWrapper::class.java.hookMethod("attachBaseContext", Context::class.java) {
+        doAfter {
+            if (thisObject is Application) {
+                unhook()
+                callback(thisObject as Application)
+            }
+
+        }
+    }
+}
+
+fun onDexClassLoaderLoaded(callback: ClassLoader.(unhook: () -> Unit) -> Unit) {
+    var unhooks: Set<Unhook>? = null
+
+    unhooks = BaseDexClassLoader::class.java.hookAllConstructor {
+        doAfter {
+            val hookContext = this@hookAllConstructor
+
+            hookContext.doOnce(thisObject) {
+                callback(thisObject as ClassLoader) {
+                    unhooks?.forEach { it.unhook() }
+                }
+            }
+        }
+    }
+}
