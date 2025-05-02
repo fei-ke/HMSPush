51470575343d35fdd8cb4232f264c44d0aa30d74
Goooler <wangzongler@gmail.com>
2025-05-02 21:46:18 +0800

    Request POST_NOTIFICATIONS permission on Android 13+ devices

--- app/src/main/java/one/yufz/hmspush/app/MainActivity.kt
+++ app/src/main/java/one/yufz/hmspush/app/MainActivity.kt
@@ -1,8 +1,13 @@
 package one.yufz.hmspush.app
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.view.WindowCompat
 import one.yufz.hmspush.app.theme.AppTheme
 
@@ -14,6 +19,20 @@ class MainActivity : ComponentActivity() {
             AppTheme {
                 AppNavHost()
             }
+        }
+
+        requestNotificationPermission()
+    }
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
+                .launch(Manifest.permission.POST_NOTIFICATIONS)
         }
     }
 }
